47d2989f7b3edca345906230fb033751221bc4d1
Dani Dominguez <dani.d.zamorano@gmail.com>
2024-01-12 16:12:21 +0100

    Update video

--- app/[locale]/ui/Video.tsx
+++ app/[locale]/ui/Video.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from "next-intl";
 import Button from "@/ui/Button";
+import Logo from "@/ui/Logo";
 import styles from "./Video.module.css";
 import { Jura } from "next/font/google";
 
@@ -13,44 +14,57 @@ export default function Video() {
 
   return (
     <div
-      className={`${jura.className}relative h-[80vh] w-full max-w-[100vw] md:aspect-video md:h-full`}
+      className={`${jura.className} relative h-dvh w-full max-w-[100vw] md:aspect-video md:h-[80vh] xl:h-full`}
     >
-      <div className="absolute left-1/2 right-0 top-14 z-10 m-auto flex -translate-x-1/2 flex-col items-center w-full">
-        <section className="animate-fade-up flex justify-center items-center gap-8 flex-col aspect-[343/350] md:aspect-[343/160] 2xl:aspect-[343/260]">
+      <div className="absolute inset-0 h-full justify-center z-10 m-auto flex flex-col items-center w-full">
+        <section className="absolute z-10 animate-fade-up flex justify-center items-center gap-8 flex-col">
           <h1 className="flex items-center gap-5 text-4xl md:text-6xl lg:gap-20 lg:text-8xl font-tomaso">
             <span>E</span>
             <span>S</span>
             <span>L</span>
-            <span className={styles.letterLogo} aria-label="A"></span>
+            <Logo
+              className="h-9 w-auto md:h-[3.75rem] lg:h-24 scale-150"
+              aria-label="A"
+            />
             <span>N</span>
             <span>D</span>
           </h1>
-          <p className="text-xl lg:text-2xl text-center text-pretty font-jura">
+
+          <p className="subtitle text-xl lg:text-2xl text-center text-wrap font-jura px-4">
             {t("HERO_SUBTITLE")}
           </p>
-        </section>
-        <Button
-          className="animate-fade-up"
-          url="https://www.youtube.com/watch?v=1YmpPrqKP-g"
-        >
-          {t("VIDEO_BUTTON")}
-        </Button>
-      </div>
 
+          <Button
+            target="_self"
+            className="text-sky-950 animate-fade-up bg-white"
+            url="/vota"
+          >
+            {t("VIDEO_BUTTON")}
+          </Button>
+        </section>
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          id="main-video"
+          preload="metadata"
+          className="video cp-v h-full w-full object-cover object-top animate-duration-[2s] animate-ease-in-out"
+        >
+          <source src="/video.webm" type="video/webm" />
+          <source src="/video.mp4" type="video/mp4" />
+        </video>
+      </div>
       <video
-        className="cp-v h-full w-full animate-fade object-cover object-top animate-duration-1000 animate-ease-in-out"
-        src="/video.webm"
+        className="video absolute -bottom-48 -z-40 blur-3xl animate-duration-[2s] animate-ease-in-out"
         autoPlay
         loop
         muted
-      ></video>
-      <video
-        className="absolute -bottom-48 -z-40 blur-3xl"
-        src="/video.webm"
-        autoPlay
-        loop
-        muted
-      ></video>
+        preload="metadata"
+      >
+        <source src="/video.webm" type="video/webm" />
+        <source src="/video.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
